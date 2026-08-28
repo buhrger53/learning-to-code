1d69982e3a869065b45ef0fa4956cdebe9164e99
burger123453 <125590920+buhrger53@users.noreply.github.com>
2026-08-28 10:44:17 -0700

    experiment: input stuff
    - also learning about some functions and classes

--- scripts/sprite_2d.gd
+++ scripts/sprite_2d.gd
@@ -1,18 +1,52 @@
 extends Sprite2D
 
+
+## speed in units ps
 var speed: int = 400
-
+## rotation speed in radians ps
 var angular_speed: float = PI
-
+## variable to apply velocity to position
 var velocity: Vector2
+## is the direction 1 or -1
+var direction: Vector2
 
 # runs on initiation of the Sprite2D node
 func _init() -> void: 
+	# idk
 	print("hello world")
 
-
+# runs every frame
 func _process(delta: float) -> void: 
-	rotation += angular_speed * delta
+	velocity = Vector2.ZERO
 	
-	velocity = Vector2.UP.rotated(rotation) * speed
+	for string: StringName in ["ui_left", "ui_right", "ui_up", "ui_down"]:
+		match string:
+			"ui_left":
+				direction = Vector2.LEFT
+			"ui_right":
+				direction = Vector2.RIGHT
+			"ui_up":
+				direction = Vector2.UP
+			"ui_down":
+				direction = Vector2.DOWN
+		if Input.is_action_pressed(string):
+			velocity = direction * speed
+	
+	
+	
 	position += velocity * delta
+
+func _unhandled_input(event: InputEvent) -> void:
+	print("event gotten")
+	# what was the other way to uh get the things?
+	if event.as_text() == "Alt":
+		print("alt pressed")
+	if event is InputEventKey:
+		handle_keys(event)
+
+func handle_keys(key: InputEventKey) -> void:
+	match key.keycode:
+		KEY_SPACE when key.pressed:
+			print("space")
+		KEY_SPACE when not key.pressed:
+			print("unspace")
