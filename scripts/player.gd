2c6dcc96b83f150547bcf860ac3299ea4a3e5d2b
burger123453 <125590920+buhrger53@users.noreply.github.com>
2026-08-28 18:05:10 -0700

    experiment: random player thing
    - i could've committed at a better time
    - this is right before the signals part

---
+++ scripts/player.gd
@@ -0,0 +1,58 @@
+extends Area2D
+
+## speed variable that can be changed in inspector
+## i have to read the style guide
+## maybe?
+## i could just use a linter
+@export var speed: int = 400
+
+var screen_size: Vector2
+## the direction because vector2 that we move
+## it's length is unit
+## because of later code
+var move_direction: Vector2
+
+var velocity: Vector2
+
+@onready var animated_sprite_2d: Node = $AnimatedSprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# hide()
+	screen_size = get_viewport_rect().size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	velocity = Vector2.ZERO
+
+	move_direction = Input.get_vector(
+			"move_left", "move_right", 
+			"move_up", "move_down"
+	).normalized();
+	if move_direction.length() > 0:
+		velocity = move_direction * speed
+		if animated_sprite_2d: animated_sprite_2d.play()
+	else:
+		if animated_sprite_2d: animated_sprite_2d.stop()
+
+	if velocity == Vector2.ZERO:
+		animated_sprite_2d.animation = "walk"
+		animated_sprite_2d.flip_v = false
+	
+	if animated_sprite_2d:
+		if velocity.x != 0:
+			animated_sprite_2d.animation = "walk"
+			animated_sprite_2d.flip_v = false
+			# left
+			animated_sprite_2d.flip_h = velocity.x < 0
+		elif velocity.y != 0:
+			animated_sprite_2d.animation = "up"
+			animated_sprite_2d.flip_h = false
+			# down
+			animated_sprite_2d.flip_v = velocity.y > 0
+
+	
+	position += velocity * delta
+	position.clamp(Vector2.ZERO, screen_size)
